a8a646aef33bc6101e1a024fc2518b01d204de58
UltimateGG18 <gauravgaonkar1998@gmail.com>
2021-09-08 16:36:14 +0530

    create new restaurant api and filter api

--- src/routes/restaurant.route.js
+++ src/routes/restaurant.route.js
@@ -6,4 +6,8 @@ module.exports = (app) => {
     app.get("/getrestaurantsbylocation/:locationId", restaurantController.getRestaurantsByLocation);
     //to get restaurant details by its id
     app.get("/getrestaurantdetailsbyid/:restaurantId", restaurantController.getRestaurantDetailsById);
+    //to filter restaurants
+    app.post("/filterrestaurants", restaurantController.filterRestaurants);
+    //to add new restaurant
+    app.post("/addnewrestaurant", restaurantController.addNewRestaurant);
 };
